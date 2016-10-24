9741a067e52cde5649f1474bb23023f045a19c3f
XeliteXirish <shaunoneill7@me.com>
2016-10-24 12:58:44 +0100

    Revamped navigation drawer

--- app/src/main/java/com/scammersublounge/sslapp/MainActivity.java
+++ app/src/main/java/com/scammersublounge/sslapp/MainActivity.java
@@ -1,101 +1,74 @@
 package com.scammersublounge.sslapp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.scammersublounge.sslapp.utils.NavigationHandler;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+
+    private AccountHeader mNavDrawerHeader = null;
+    private Drawer mNavDrawer = null;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // Handle Toolbar
+        mToolbar = (Toolbar) findViewById(R.id.toolbar_main);
+        setSupportActionBar(mToolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
+        // Create the account header
+        buildNavHeader(false, savedInstanceState);
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        // Create the nav drawer
+        buildNavDrawer(savedInstanceState);
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
+    private void buildNavHeader(boolean compact, Bundle savedInstanceState) {
+        mNavDrawerHeader = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.nav_header_background)
+                .withCompactStyle(compact)
+                .withSavedInstance(savedInstanceState)
+                .build();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
+    private void buildNavDrawer(Bundle savedInstanceState) {
+        mNavDrawer = new DrawerBuilder()
+                .withActivity(this)
+                .withAccountHeader(mNavDrawerHeader)
+                .withToolbar(mToolbar)
+                .withActionBarDrawerToggleAnimated(true)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName("Home").withIdentifier(NavigationHandler.idHome),
+                        new PrimaryDrawerItem().withName("About").withIdentifier(NavigationHandler.idAbout)
+                )
+                .addStickyDrawerItems(
+                        new SecondaryDrawerItem().withName("Settings").withIcon(GoogleMaterial.Icon.gmd_settings).withIdentifier(NavigationHandler.idSettings)
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        NavigationHandler.handleClick(MainActivity.this, drawerItem);
+                        return false;
+                    }
+                })
+                .withSavedInstance(savedInstanceState)
+                .build();
     }
 }
---
+++ app/src/main/java/com/scammersublounge/sslapp/utils/NavigationHandler.java
@@ -0,0 +1,49 @@
+package com.scammersublounge.sslapp.utils;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
+
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.scammersublounge.sslapp.R;
+
+/**
+ * Created by XeliteXirish on 24/10/2016 (www.xelitexirish.com)
+ */
+public class NavigationHandler {
+
+    private static final String TAG = NavigationHandler.class.getSimpleName();
+
+    public static final int idHome = 1;
+    public static final int idAbout = 2;
+    public static final int idSettings = 3;
+
+    public static void handleClick(Context context, IDrawerItem drawerItem) {
+        long id = drawerItem.getIdentifier();
+
+        if (id == idHome){
+
+        }else if (id == idAbout){
+
+        }else if (id == idSettings){
+
+        }
+    }
+
+    public static void switchScreen(Context context, Object screen) {
+        if (screen instanceof Fragment) {
+            try {
+                FragmentManager fragmentManager = ((FragmentActivity) context).getSupportFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.fragment_container, (Fragment) screen).addToBackStack(null).commit();
+            } catch (ClassCastException e) {
+                Log.e(TAG, "Can't get fragment manager");
+            }
+
+        } else if (screen instanceof Intent) {
+            context.startActivity((Intent) screen);
+        }
+    }
+}
